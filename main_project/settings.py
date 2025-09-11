1a1ad75482738ca5f44ab0991dcf45e07717f200
Tanish Dhar <tanishd.mt@Shapoorji.com>
2025-09-11 05:19:06 -0700

    Fix: Add dj_database_url import for deployment

--- main_project/settings.py
+++ main_project/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from dotenv import load_dotenv
-load_dotenv()
+import dj_database_url
 from pathlib import Path
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
